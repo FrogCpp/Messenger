5fa1d10c940a435d5e06f871b7b0a80783e23bc8
FrogCpp <161460644+FrogCpp@users.noreply.github.com>
2025-03-29 21:40:10 +0300

    execute command

--- Messenger/Messenger/MySql/SQLiteUser.cs
+++ Messenger/Messenger/MySql/SQLiteUser.cs
@@ -13,10 +13,53 @@ namespace Messenger
         {
             using (var connection = new SqliteConnection("Data Source=testDatabase.db"))
             {
+                execute("CREATE TABLE IF NOT EXISTS Users (UserName STRING, UserLogin STRING, UserPasword STRING, MemberChats STRING)");
+            }
+        }
+
+        private string executeScalar(string commandS, string param="nonQuery")
+        {
+            string answer = "";
+            using (var connection = new SqliteConnection("Data Source=testDatabase.db"))
+            {
                 connection.Open();
                 var command = connection.CreateCommand();
-                command.CommandText = "CREATE TABLE IF NOT EXISTS Users (UserName STRING, UserLogin STRING, UserPasword STRING, MemberChats STRING)";
+                command.CommandText = commandS;
+                object ans = command.ExecuteScalar();
+                answer = ans.ToString();
+            }
+            return answer;
+        }
+        private string[] executeReader(string commandS)
+        {
+            string[] answer;
+            using (var connection = new SqliteConnection("Data Source=testDatabase.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = commandS;
+                using (var reader = command.ExecuteReader())
+                {
+                    answer = new string[reader.FieldCount];
+                    int i = 0;
+                    while (reader.Read())
+                    {
+                        answer[i] = reader.GetString(i++);
+                    }
+                }
+                connection.Close();
+            }
+            return answer;
+        }
+        private void executeNonQuery(string commandS)
+        {
+            using (var connection = new SqliteConnection("Data Source=testDatabase.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = commandS;
                 command.ExecuteNonQuery();
+                connection.Close();
             }
         }
     }
