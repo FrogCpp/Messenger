a4dcfd0cc4a55e92ea5cf4a8e8a4bb3a79cbad3b
Gyro <gyroscopic.why@gmail.com>
2025-03-29 03:24:28 +0300

    Create database structure
    Uploaded a png file of the database structure

--- MessengerClient/Program.cs
+++ MessengerClient/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+
+using static System.Console;              //  Для удобства, чтобы не писать Console. каждый раз
+
+//using System.Collections.Generic;       //  Пока не надо
+//using System.Linq;                      //
+//using System.Security.Cryptography;     //
+//using System.Threading.Tasks;           //
+
+
 
 /*
- * важно уточнить, перед работой клиента каждый раз надо прокидывать базавую конфигуряцию клиента
+ * важно уточнить, перед работой клиента каждый раз надо прокидывать базовую конфигуряцию клиента
  * комманды для конфигурации:
  * setName(
- * задает имя полльзователя на стороне сервера
+ * задает имя пользователя на стороне сервера
  * )
  * 
  * setLogin(
@@ -24,16 +29,16 @@ using System.Threading.Tasks;
  * 
  * если на стороне сервера имеется аккаунт удовлетворяющий всем трем параметрам выше, то дальше работа идет с ним
  * 
- * getHistori(
- * после опрееления клиента клиенту копируется история чатов. Подробнее о синтасисе передачи в функция этому посвещенных
+ * getHistory(
+ * после определения клиента клиенту копируется история чатов. Подробнее о синтасисе передачи в функция этому посвещенных
  * )
  * комманды для работы:
  * sendMessage: ***(
- * *** - это аргумент (сама мессага) с фотками буде прикол а с видео и подавно
+ * *** - это аргумент (сама мессага) с фотками будет прикол а с видео и подавно,  ууу, готовлюсь :/
  * )
  * 
  * updateChat(
- * обнавление чата, выщывается чуть ли не постоянно, но работает только если чат на стороне сервера изменен
+ * обновление чата, выщитывается чуть ли не постоянно, но работает только если чат на стороне сервера изменен
  * )
  */
 
@@ -41,39 +46,9 @@ namespace MessengerClient
 {
     internal class Program
     {
-        static void Main(string[] args) // тестовая работа, клиента потом перепишу
+        static void Main() // тестовая работа, клиента потом перепишу  - Давай 
         {
-            Socket mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                mainSocket.Connect(IPAddress.Parse("127.0.0.1"), 8000);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            string message;
-            byte[] buffer = new byte[1024];
-            int br = 0;
-            while (true)
-            {
-                message = Console.ReadLine();
-
-                mainSocket.Send(Encoding.ASCII.GetBytes(message));
-
-                buffer = new byte[0];
-                br = 1024;
-                while (br == 1024)
-                {
-                    byte[] reader = new byte[1024];
-                    br = mainSocket.Receive(reader);
-                    byte[] bts = new byte[buffer.Length + reader.Length];
-                    Buffer.BlockCopy(buffer, 0, bts, 0, buffer.Length);
-                    Buffer.BlockCopy(reader, 0, bts, buffer.Length, reader.Length);
-                    buffer = bts;
-                }
-                Console.WriteLine(Encoding.ASCII.GetString(buffer));
-            }
+            Write("Aboba");
         }
     }
 }
