662daf02773a711f6ac82d83aef1c06bde026281
a <161460644+FrogCpp@users.noreply.github.com>
2025-12-15 20:03:40 +0300

    plus user

--- Messenger/MessengerServer/Program.cs
+++ Messenger/MessengerServer/Program.cs
@@ -9,10 +9,16 @@ namespace MessengerServer
         /*прим. для себя: здесь называем переменные так: _a - приватные; a - публичные; A - функции и классы*/
 
         public static readonly ConcurrentQueue<UserTask> TaskList = new();
+        public static readonly ConcurrentDictionary<int, User> UserList = new();
 
         static void Main(string[] args)
         {
 
         }
+    }
+
+    struct User 
+    {
+        public int a;
     }
 }
--- Messenger/MessengerServer/UserTask.cs
+++ Messenger/MessengerServer/UserTask.cs
@@ -37,28 +37,6 @@ namespace MessengerServer
             content = cnt;
         }
 
-        public bool CompleteProcessing(MessageSample msg)
-        {
-            byte[] msgB = Encoding.Unicode.GetBytes(JsonSerializer.Serialize<MessageSample>(msg));
-
-            MessageSample a = new();
-            a.type = TaskType.MSG_LENGHT;
-            a.content = BitConverter.GetBytes(msgB.Length);
-
-            socket.Send(Encoding.Unicode.GetBytes(JsonSerializer.Serialize<MessageSample>(a)));
-
-            byte[] buffer = new byte[1];
-
-            socket.Receive(buffer);
-
-            if (buffer[0] == 0)
-            {
-                return false;
-            }
-
-            socket.Send(msgB);
-
-            return true;
-        }
+        
     }
 }
