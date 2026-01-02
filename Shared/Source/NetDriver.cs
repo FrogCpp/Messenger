fa501ee84990853e94ac39445ed4a229fbf0900f
a <161460644+FrogCpp@users.noreply.github.com>
2026-01-03 01:00:18 +0300

    writing NetDriver

--- Shared/Source/NetDriver.cs
+++ Shared/Source/NetDriver.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static JabNet.NetDriver.Package;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace JabNet
@@ -164,9 +172,13 @@ namespace JabNet
                 packageID = id ?? Guid.NewGuid();
             }
 
-            public void Add(PackageElement pkge)
+            public bool Add(PackageElement pkge)
             {
+                if (pkge.packageID != packageID) return false;
+                if (pkge.packageLength == pack.Count) return false;
                 pack.Add(pkge);
+
+                return true;
             }
         }
 
@@ -206,6 +218,8 @@ namespace JabNet
                 var data = new byte[pack.packageLength];
                 int offset = 0;
 
+                pack.pack.Sort((a, b) => a.elementIndex.CompareTo(b.elementIndex));
+
                 for (int i = 0; i < pack.pack.Count; i++)
                 {
                     Buffer.BlockCopy(pack.pack[i].content, 0, data, offset, pack.elementLength);
@@ -219,17 +233,88 @@ namespace JabNet
 
 
 
-        public static class EventController
+        internal static class EventController
         {
-            public static async Task Listen(List<Task> tasks, Action<Task> act)
+            public static async Task Listen(List<Task> tasks, Func<Task, Task> act)
             {
                 while (tasks.Count != 0)
                 {
                     var tsk= Task.WhenAny(tasks);
 
-                    act(tsk);
+                    await act(tsk);
                 }
             }
+        }
+
+        public class ConnectionController
+        {
+            public enum c_state
+            {
+                user,
+                server,
+            }
+
+            private readonly c_state _state;
+            private ConcurrentBag<User> _userList;
+
+            public ConnectionController(ConcurrentBag<User> usrlst, c_state state = c_state.user)
+            {
+                _state = state;
+                _userList = usrlst;
+            }
+
+            public async Task AsyncAcceptNewClients()
+            {
+                if (_state == c_state.user) return;
+
+                Socket listner = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listner.Bind(new IPEndPoint(IPAddress.Any, 121221));
+                listner.Listen();
+
+                while (true)
+                {
+                    var clientSocket = await listner.AcceptAsync();
+
+                    _userList.Add(new User(clientSocket));
+                    var cl = _userList.First(s => s.socket == clientSocket);
+                    cl.ListenTask = clientSocket.ReceiveAsync(cl.buffer);
+                }
+            }
+
+            public async Task ListenAsync()
+            {
+                await EventController.Listen(_userList.Select(user => user.ListenTask).ToList(), ReciveMessage);
+            }
+
+            public async Task ReciveMessage(Task reciving)
+            {
+                var usr = _userList.First(t => t.ListenTask == reciving);
+
+                PackageElement pkge = JsonSerializer.Deserialize<PackageElement>(Encoding.Unicode.GetString(usr.buffer));
+
+                var pack = usr.incoming.SingleOrDefault(p => p.packageID == pkge.packageID);
+                if (pack.Equals(default(PackageElement)))
+                {
+                    var pkg = new Package();
+                    pkg.Add(pkge);
+                    usr.incoming.Add(pkg);
+                }
+                else
+                {
+                    pack.Add(pkge);
+                }
+            }
+        }
+
+
+        public class User(Socket sk)
+        {
+            public readonly Socket socket = sk;
+            public Task ListenTask;
+            public readonly byte[] buffer = new byte[128 * 1024];
+
+            public readonly List<Package> incoming = new();
+            public readonly List<Package> outgoing = new();
         }
     }
 }
