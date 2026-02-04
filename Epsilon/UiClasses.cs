7364cca260b9255c3def30f67713fbcce4f4194a
a <161460644+FrogCpp@users.noreply.github.com>
2026-02-04 21:36:28 +0300

    any code
    without links and builds

---
+++ Epsilon/UiClasses.cs
@@ -0,0 +1,117 @@
+﻿using DateTime4bLib;
+using Observables.Specialized.Extensions;
+using Shared.Source;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Epsilon
+{
+    internal class Accaunt : JN_Author, INotifyPropertyChanged
+    {
+        public Accaunt(string name, string surname, string bio, ulong suid, ImageSource avatar) : base(name, surname, bio, suid, avatar)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.bio = bio;
+
+            this.suid = suid;
+            this.avatar = avatar;
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Surname
+        {
+            get => surname;
+            set
+            {
+                surname = value;
+                OnPropertyChanged();
+            }
+        }
+        public ImageSource Avatar
+        {
+            get => avatar;
+            set
+            {
+                avatar = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+    internal class Chat : JN_Chat, INotifyPropertyChanged
+    {
+        private Dictionary<UInt64, JN_Message> _chatStory;
+        public Chat(List<ulong> membersSUID, ImageSource chatAvatar) : base(membersSUID, chatAvatar)
+        {
+            this.membersSUID = membersSUID;
+            this.chatAvatar = chatAvatar;
+        }
+
+        public Dictionary<UInt64, JN_Message> ChatStory
+        {
+            get => _chatStory;
+        }
+
+        public void SendMessage(JN_Message msg)
+        {
+            _chatStory.Add(ref msg.mesageSUID, msg);
+            OnPropertyChanged();
+        }
+
+        public void RemoveMessage(UInt64 id)
+        {
+            _chatStory.Remove(id);
+            OnPropertyChanged();
+        }
+
+        public void ChangeMesage(UInt64 id, string text)
+        {
+            _chatStory[id].message = text;
+            OnPropertyChanged();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    internal class Message : JN_Message, INotifyPropertyChanged
+    {
+        public Message(DateTime4b sentTime, string message, ulong authorSUID, ulong mesageSUID) : base(sentTime, message, authorSUID, mesageSUID)
+        {
+            this.sentTime = sentTime;
+            this.message = message;
+
+            this.authorSUID = authorSUID;
+            this.mesageSUID = mesageSUID;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Shared/Source/InterfaceClasses.cs
+++ Shared/Source/InterfaceClasses.cs
@@ -9,23 +9,24 @@ using DateTime4bLib;
 
 namespace Shared.Source
 {
-    public class JN_Message(DateTime4b sentTime, string message, UInt64 authorSUID)
+    public class JN_Message(DateTime4b sentTime, string message, UInt64 authorSUID, UInt64 mesageSUID)
     {
         public DateTime4b sentTime = sentTime;
         public string message = message;
 
         public UInt64 authorSUID = authorSUID;
+        public UInt64 mesageSUID = mesageSUID;
     }
 
 
     public class JN_Author(string name, string surname, string bio, ulong suid, ImageSource avatar)
     {
-        public string name    = name;
-        public string surname = surname;
+        protected string name    = name;
+        protected string surname = surname;
         public string bio     = bio;
 
         public UInt64 suid = suid;
-        public ImageSource avatar = avatar;
+        protected ImageSource avatar = avatar;
 
 
 
@@ -33,13 +34,16 @@ namespace Shared.Source
     }
 
 
-    public class JN_Chat(List<UInt64> membersSUID, ImageSource chatAvatar, List<JN_ChatTopic> topics)
+    public class JN_Chat(List<UInt64> membersSUID, ImageSource chatAvatar)
     {
         public List<UInt64>  membersSUID = membersSUID;
         public ImageSource chatAvatar = chatAvatar;
 
         //public List<JN_ChatTopic> topics = topics;
     }
+
+
+
     public class JN_ChatTopic(ImageSource topicAvatar, string topicTitle, Int32 topicID) // значительно позже. . .
     {
         public ImageSource topicAvatar = topicAvatar;
