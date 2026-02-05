e5e24a99b488ac82a334eef8465a58f437a896ff
a <161460644+FrogCpp@users.noreply.github.com>
2026-02-05 20:42:17 +0300

    Revert "removed a useless complication"
    This reverts commit 134613b7842a4ee165746d1dc969b9c7daa386ce.

--- Epsilon/UiClasses.cs
+++ Epsilon/UiClasses.cs
@@ -9,9 +9,8 @@ using System.Text;
 
 namespace Epsilon
 {
-    public class Accaunt : JN_Author
+    public class Accaunt : JN_Author, INotifyPropertyChanged
     {
-
         public Accaunt(string name, string surname, string bio, ulong suid, ImageSource avatar) : base(name, surname, bio, suid, avatar)
         {
             this.name = name;
@@ -21,10 +20,44 @@ namespace Epsilon
             this.suid = suid;
             this.avatar = avatar;
         }
+
+        public override string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public override string Surname
+        {
+            get => surname;
+            set
+            {
+                surname = value;
+                OnPropertyChanged();
+            }
+        }
+        public override ImageSource Avatar
+        {
+            get => avatar;
+            set
+            {
+                avatar = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
 
-    public class Chat : JN_Chat
+    public class Chat : JN_Chat, INotifyPropertyChanged
     {
         public Dictionary<UInt64, JN_Message> ChatStory { get; private set; }
         public Chat(List<ulong> membersSUID, ImageSource chatAvatar, string chatName) : base(membersSUID, chatAvatar, chatName)
@@ -33,24 +66,51 @@ namespace Epsilon
             this.chatAvatar = chatAvatar;
             this.chatName = chatName;
         }
+        public override List<UInt64> MembersSUID
+        {
+            get => membersSUID;
+            set
+            {
+                membersSUID = value;
+                OnPropertyChanged();
+            }
+        }
+        public override string ChatName
+        {
+            get => base.ChatName;
+            set
+            {
+                chatName = value;
+                OnPropertyChanged();
+            }
+        }
 
         public override void SendMessage(JN_Message msg)
         {
             ChatStory.Add(msg.mesageSUID, msg);
+            OnPropertyChanged();
         }
 
         public override void RemoveMessage(UInt64 id)
         {
             ChatStory.Remove(id);
+            OnPropertyChanged();
         }
 
         public override void ChangeMessage(UInt64 id, string text)
         {
-            ChatStory[id].message = text;
+            ChatStory[id].MessageText = text;
+            OnPropertyChanged();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
-    public class Message : JN_Message
+    public class Message : JN_Message, INotifyPropertyChanged
     {
         public Message(DateTime4b sentTime, string message, ulong authorSUID, ulong mesageSUID) : base(sentTime, message, authorSUID, mesageSUID)
         {
@@ -62,5 +122,23 @@ namespace Epsilon
         }
 
         public string StringSentTime { get => sentTime.CurrentToStandardString(); }
+
+        public override string MessageText
+        {
+            get => message;
+            set
+            {
+                message = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
