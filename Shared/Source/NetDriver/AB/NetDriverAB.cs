70d1d5e152edabd67843b13998c7f68c7304631f
a <161460644+FrogCpp@users.noreply.github.com>
2026-03-07 00:50:29 +0300

    some code

--- Shared/Source/NetDriver/AB/NetDriverAB.cs
+++ Shared/Source/NetDriver/AB/NetDriverAB.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Source.NetDriver.AB
 {
-    public class ClientContorlHub(IPAddress domain, UInt16 port, Action<byte[]> processor)
+    public class ClientContorlHub
     {
-        private readonly IPAddress _domain = domain;
-        private readonly UInt16 _port = port;
-        private readonly Action<byte[]> _processor = processor;
+        private readonly IPAddress _domain;
+        private readonly UInt16 _port;
+        private readonly Socket _serverSock = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly List<Task> tasks = new();
+        private readonly Action<byte[]> _processor;
 
-        private async Task<byte[]> RecivingMessageAsync()
+        public ClientContorlHub(IPAddress domain, UInt16 port, Action<byte[]> processor)
         {
+            _domain = domain;
+            _port = port;
+            _processor = processor;
 
+            _ = InitalizeAsyncFunc();
+        }
+
+        private async void InitalizeAsyncFunc()
+        {
+            await _serverSock.ConnectAsync(new IPEndPoint(_domain, _port));
+        }
+
+        private async Task RecivingMessageAsync()
+        {
+            while (true)
+            {
+
+            }
         }
 
         private async Task SendMessageAsync(byte[] content)
--- Shared/obj/Release/net10.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Release/net10.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6ac7b0d3393c59c64bf38ca621bf81da950173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39ad3ac9e17fb72fba3c6184f882dd61f87811d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
