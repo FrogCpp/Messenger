8e498c23e706ba4a6ef40cadce1f6602dd8223e1
a <161460644+FrogCpp@users.noreply.github.com>
2026-03-15 16:53:32 +0300

    init

--- Shared/obj/Release/net10.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Release/net10.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b97ca34af9c9367b13561bf3313e044cc1362b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360c66279a11c86eb36c917e7422898cd2edabd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
